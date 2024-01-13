fb8363bc8f141de1156fd26ceff4ca0e0c303e13
athanasioy <athanasioy.antonis@gmail.com>
2024-01-13 18:32:09 +0200

    Restructure sql_queries folder

--- sql_queries/opengov_analysis.sql
+++
@@ -1,9 +0,0 @@
-select Case when legislation_type is null then 'Miscellaneous' else legislation_type end as LegislationType , count(*) as cnt from Legislation
-where id<=890
-group by legislation_type;
-
-/* ποια νομοσχέδια έχουν παράπανω από μία διαβουλεύσεις */
-
-SELECT Count(*) from Legislation WHERE final_legislation_id is not null
-
-SELECT COUNT(Distinct final_legislation_id) FROM Legislation WHERE final_legislation_id is not null

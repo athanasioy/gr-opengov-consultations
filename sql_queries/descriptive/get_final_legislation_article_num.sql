346c37955acd19a22d3cbd79cb4917ab601c256b
athanasioy <athanasioy.antonis@gmail.com>
2024-01-14 21:37:26 +0200

    Fix SQL Query to properly match Initial and Final Article Number
    Fixed the SQL Query that finds for each given Proposed Bill
    Article Number the corresponding Article Number in the Voted Bill.
    
    Article numbers are now cast to INTEGER beforing joining to
    the Voted Bill's  Article Number. This was done to prevent
    failures to match initial and voted Articles due to the fact
    some Proposed Bills Article Numbers had a leading zero for
    single digit numbers (i.e. '01' instead of '1')

--- sql_queries/descriptive/get_final_legislation_article_num.sql
+++ sql_queries/descriptive/get_final_legislation_article_num.sql
@@ -4,7 +4,7 @@
 		map.MaxArticleToMap,
 		FinalArticle.text,
 		--FinaLegislationArticles.Number as FinalArtNumber,
-		coalesce(row_number() OVER (PARTITION by Article.legislation_id,map.MaxArticleToMap order by CAST(Article.number AS INT)) -1 + map.MaxArticleToMap, Article.number) as joinNum
+		coalesce(row_number() OVER (PARTITION by Article.legislation_id,map.MaxArticleToMap order by CAST(Article.number AS INT)) -1 + map.MaxArticleToMap, cast(Article.number as int) as joinNum
 		
  FROM Article
  left join Legislation on Legislation.id = Article.legislation_id
@@ -14,7 +14,7 @@
 			max(ArticleMapping.final_legislation_article_no) as MaxArticleToMap
 	FROM Article
 	LEFT JOIN ArticleMapping on ArticleMapping.legislation_id = Article.legislation_id
-							AND Article.number >= ArticleMapping.public_consultation_article_no
+							AND Cast(Article.number as int) >= ArticleMapping.public_consultation_article_no
 	GROUP BY 	Article.legislation_id,
 				Article.number
  
--- sql_queries/descriptive/get_initial_and_final_text.sql
+++ sql_queries/descriptive/get_initial_and_final_text.sql
@@ -21,7 +21,7 @@ SELECT 	Article.legislation_id,
 		coalesce(row_number() OVER (PARTITION by Article.legislation_id,max(ArticleMapping.final_legislation_article_no) order by CAST(Article.number AS INT)) -1 + max(ArticleMapping.final_legislation_article_no), Article.number) as joinNum
 FROM Article	
 	INNER JOIN Legislation on Legislation.id = Article.legislation_id and Legislation.final_legislation_id is not NULL
-	LEFT JOIN ArticleMapping ON ArticleMapping.legislation_id = Article.legislation_id and Article.number >= ArticleMapping.public_consultation_article_no
+	LEFT JOIN ArticleMapping ON ArticleMapping.legislation_id = Article.legislation_id and Cast(Article.number as int) >= ArticleMapping.public_consultation_article_no
 GROUP by Article.legislation_id,Article.number)
 AS A
 ON A.legislation_id = Article.legislation_id and A.number = Article.number

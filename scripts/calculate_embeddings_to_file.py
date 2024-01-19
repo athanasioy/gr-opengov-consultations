e081cbe5bf005d7689bbbf714ace4545f5d35be4
athanasioy <athanasioy.antonis@gmail.com>
2024-01-19 21:31:26 +0200

    Minor changes to calculate embeddings to file script
    Added the output_dir_path when printing incorrent
    output directories.
    
    Fix indentation from 2 spaces to 4 spaces.

--- scripts/calculate_embeddings_to_file.py
+++ scripts/calculate_embeddings_to_file.py
@@ -12,7 +12,8 @@ from text_utils.textEmbeddings import GreekLongFormerEncoder
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("csv_file", help="Relative Path of the csv file. The CSV File must contain Id and Text as Columns")
+parser.add_argument("csv_file",
+                    help="Relative Path of the csv file. The CSV File must contain Id and Text as Columns")
 parser.add_argument("output_dir",help= "Output directory of text embeddings.")
 parser.add_argument("-t", "--test", action="store_true")
 parser.add_argument("-b", "--batch_size", default=4, type=int)
@@ -33,7 +34,7 @@ if not csv_file_path.is_file():
     raise SystemExit(2)
 
 if not output_dir_path.is_dir() and output_dir_path.exists():
-    print(f"output directory path must be a directory")
+    print(f"output directory path {output_dir_path} must be a directory")
     raise SystemExit(2)
 
 if not output_dir_path.exists():
@@ -50,14 +51,14 @@ ids = df['articleId'].astype(int).tolist()
 texts = df['text'].tolist()
 
 while len(texts)>0:
-  texts_batch = texts[:BATCH_SIZE]
-  ids_batch = ids[:BATCH_SIZE]
-  embeddings, token_sizes = encoder.get_embeddings_batch(texts_batch)
+    texts_batch = texts[:BATCH_SIZE]
+    ids_batch = ids[:BATCH_SIZE]
+    embeddings, token_sizes = encoder.get_embeddings_batch(texts_batch)
 
-  for embedding,token_size,id in zip(embeddings,token_sizes, ids_batch):
-    np.save(output_dir_path / f"{id}_{token_size}.npy", embedding)
+    for embedding,token_size,id in zip(embeddings,token_sizes, ids_batch):
+        np.save(output_dir_path / f"{id}_{token_size}.npy", embedding)
 
-  del texts[:BATCH_SIZE]
-  del ids[:BATCH_SIZE]
-  if test_mode:
-      break
+    del texts[:BATCH_SIZE]
+    del ids[:BATCH_SIZE]
+    if test_mode:
+        break
